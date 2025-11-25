55a8cc36347651ab43df2df6b017716ae69e1028
Matheus Fernando Ribeiro Martins <matheus1030br@gmail.com>
2025-11-25 11:10:12 -0300

    Alerta ao sair do app

--- Front-End/src/pages/sair/MenuSair.js
+++ Front-End/src/pages/sair/MenuSair.js
@@ -1,31 +1,41 @@
 import Layout from '../../components/Layout';
 import icons from "../../components/Icons";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './sair.css';
 
 function MenuSair() {
-    return (
-            <main className="content">
-                <section className="form-section">
-                    <div className="section-header">
-                        <span className="icon"><i className={icons.sair}></i></span>
-                        <h3>Loggof</h3>
-                    </div>
-                    <hr className="divider" />
-                    <h4>Tem certeza de que deseja sair agora?</h4>
-                </section>
+    const navigate = useNavigate();
 
-                <section className="form-section">
-                    <div className="botoes">
-                        <Link to='/app'>
-                            <button className="verde">Voltar para a Página Inicial</button>
-                        </Link>
-                        <Link to='/'>
-                            <button className="vermelho">Fazer Logout / Sair</button>
-                        </Link>
-                    </div>
-                </section>
-            </main>
+    function logout() {
+        alert("Usuário Desconectado com Sucesso!");
+        navigate('/');
+    }
+
+    return (
+        <main className="content">
+            <section className="form-section">
+                <div className="section-header">
+                    <span className="icon"><i className={icons.sair}></i></span>
+                    <h3>Logout</h3>
+                </div>
+                <hr className="divider" />
+                <h4>Tem certeza de que deseja sair agora?</h4>
+            </section>
+
+            <section className="form-section">
+                <div className="botoes">
+                    
+                    <Link to='/app'>
+                        <button className="verde">Voltar para a Página Inicial</button>
+                    </Link>
+
+                    <button className="vermelho" onClick={logout}>
+                        Fazer Logout / Sair
+                    </button>
+
+                </div>
+            </section>
+        </main>
     );
 }
 
